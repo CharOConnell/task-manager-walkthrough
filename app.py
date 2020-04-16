b1fd5aaba2a44323ce5a3d9d87ca4c59aed27dff
CharOConnell <charlotteoconnell@virginmedia.com>
2020-04-16 10:16:32 +0000

    Added templates, gitignore, URI, some data from the database, accordian menu

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+MONGO_URI_TEST = os.getenv("MONGO_URI")
+app.config["MONGO_URI"] = MONGO_URI_TEST
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template('tasks.html', tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP',"0.0.0.0"), port=int(os.getenv('PORT',"5000")), debug=True)
+    app.run(host=os.getenv('IP', "0.0.0.0"), port=int(
+        os.getenv('PORT', "5000")), debug=True)

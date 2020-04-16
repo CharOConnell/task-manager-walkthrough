980a104107c033de96d9b0b506fe262e093b2b3a
CharOConnell <charlotteoconnell@virginmedia.com>
2020-04-16 12:12:51 +0000

    Added the new task into the tasks

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI','mongodb://localhost')
 
@@ -12,6 +11,7 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
+
 @app.route('/get_tasks')
 def get_tasks():
     return render_template('tasks.html', tasks=mongo.db.tasks.find())
@@ -20,6 +20,11 @@ def get_tasks():
 def add_task():
     return render_template('addtask.html', categories=mongo.db.categories.find())
 
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP','0.0.0.0'), 
